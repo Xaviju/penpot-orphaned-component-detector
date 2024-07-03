45bb3a50891886d38db5ceaaa429503752286e1d
Xaviju <xaviju@gmail.com>
2024-07-03 14:51:50 +0200

    Icons update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import "./style.css";
 
 const root = document.querySelector<HTMLDivElement>("#app")!;
 
+const icons = {
+  locate: `
+  <svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="m8.0011-3e-8c-4.4117 0-8.0011 3.5895-8.0011 8.0011 0 4.4117 3.5895 7.9989 8.0011 7.9989 4.4112-6.59e-4 7.9989-3.5877 7.9989-7.9989 0-3.9948-3.5561-7.9793-7.9989-8.0011zm-0.55811 1.2296v2.3066c0 0.30824 0.24988 0.55811 0.55811 0.55811 0.30824 0 0.55811-0.24988 0.55811-0.55811v-2.3066c3.3375 0.28157 5.9301 2.8758 6.2111 6.2134h-2.3043c-0.30824 0-0.55811 0.24988-0.55811 0.55811s0.24988 0.55811 0.55811 0.55811h2.3043c-0.28108 3.3377-2.8735 5.932-6.2111 6.2134v-2.3066c0-0.30824-0.24988-0.55811-0.55811-0.55811-0.30823 0-0.55811 0.24988-0.55811 0.55811v2.3066c-3.3381-0.28097-5.9323-2.8753-6.2134-6.2134h2.3066c0.30824 0 0.55811-0.24988 0.55811-0.55811s-0.24988-0.55811-0.55811-0.55811h-2.3066c0.28112-3.338 2.8754-5.9323 6.2134-6.2134z" color="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.1162" style="-inkscape-stroke:none"/>
+  </svg>`,
+  component: `
+  <svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="m6.8764 0.98119a1.5903 1.5896 0 0 1 2.2471 0l5.8987 5.8958c0.61961 0.62034 0.61961 1.6257 0 2.246l-5.8987 5.8958c-0.62064 0.61931-1.6265 0.61931-2.2471 0l-5.8987-5.8958a1.5903 1.5896 0 0 1 0-2.246zm0.77968 4.8678-1.8082 1.8073a0.48536 0.48512 0 0 0 0 0.68743l1.8082 1.8073a0.48536 0.48512 0 0 0 0.68777 0l1.8082-1.8073a0.48536 0.48512 0 0 0 0-0.68743l-1.8082-1.8073a0.48536 0.48512 0 0 0-0.68777 0z" fill="none" stroke="#000" stroke-linecap="round" stroke-width="1.0324"/>
+  </svg>
+`,
+};
+
 root.innerHTML = `
 <main class="orphaned-list-wrapper">
 <div class="orphaned-counter body-s" id="comp-count"></div>
@@ -70,7 +82,7 @@ window.addEventListener("message", (event) => {
 
         // Create and append icon
         const componentListItemIcon = document.createElement("span");
-        componentListItemIcon.classList.add("icon", "icon-copy");
+        componentListItemIcon.innerHTML = icons.component;
         componentListItem.appendChild(componentListItemIcon);
 
         // Create and append icon
@@ -92,7 +104,7 @@ window.addEventListener("message", (event) => {
           "secondary"
         );
         componentListItemLocateButton.setAttribute("data-shape", shape.id);
-        componentListItemLocateButton.innerHTML = "Go";
+        componentListItemLocateButton.innerHTML = icons.locate;
         componentListItem.appendChild(componentListItemLocateButton);
 
         orphanedComponentList.appendChild(componentListItem);
